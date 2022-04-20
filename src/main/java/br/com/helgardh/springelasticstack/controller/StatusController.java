5d6febbdbedd2e5f5d7e0999efb74ef8b926962c
Joseph Alexandre <j.alexandremmfreitas@gmail.com>
2022-04-20 17:01:22 -0300

    ajustes

---
+++ src/main/java/br/com/helgardh/springelasticstack/controller/StatusController.java
@@ -0,0 +1,15 @@
+package br.com.helgardh.springelasticstack.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/status")
+public class StatusController {
+
+    @GetMapping
+    public String getStatus(){
+        return "I'm alive!";
+    }
+}
